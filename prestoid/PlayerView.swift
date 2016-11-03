951f38c4a2e55a1d868955e22a33c9a79d51a096
iAlexander <alexander@iashchuk.com>
2016-11-03 21:11:14 +0200

    Added the movie view screen and all needed assets

---
+++ prestoid/PlayerView.swift
@@ -0,0 +1,31 @@
+//
+//  PlayerView.swift
+//  prestoid
+//
+//  Created by Alexander Iashchuk on 11/3/16.
+//  Copyright © 2016 Alexander Iashchuk. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+/// A simple `UIView` subclass that is backed by an `AVPlayerLayer` layer.
+class PlayerView: UIView {
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        
+        set {
+            playerLayer.player = newValue
+        }
+    }
+    
+    var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    override class var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+}
