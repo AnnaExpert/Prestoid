6421095a5b6a498e1e8e2870627ca5d9f70f6e04
iAlexander <alexander@iashchuk.com>
2017-02-08 18:37:49 +0200

    Updated project settings

--- prestoid/AppDelegate.swift
+++ prestoid/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         
         // MARK: WARNING: Do not forget to insert the Dropbox application API key here, and in the Info.plist file
         
-        DropboxClientsManager.setupWithAppKey("<! YOUR DROPBOX APPLICATION API KEY !>")
+        DropboxClientsManager.setupWithAppKey("<!!! YOUR DROPBOX APPLICATION API KEY !!!>")
         return true
     }
     
