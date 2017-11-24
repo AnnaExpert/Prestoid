4c7256a831380813783defb54764aad608b50743
iAlexander <alexander@iashchuk.com>
2017-11-24 14:21:39 +0200

    Updated the Dropbox API to latest version

--- prestoid/AppDelegate.swift
+++ prestoid/AppDelegate.swift
@@ -1,7 +1,7 @@
 //
 //  AppDelegate.swift
 //  Prestoid - Dropbox sync video camera app with speech to text recognition
-//  Application version 1.3, build 34, 2017.06.28
+//  Application version 2.0, build 5, 2017.11.24
 //
 //  Created by Alexander Iashchuk on 11/2/16.
 //  Copyright © 2016 Alexander Iashchuk (iAlexander), http://iashchuk.com
