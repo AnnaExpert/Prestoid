daef81f335bdac2b4e0a8d44eda69636a117c3ff
iAlexander <alexander@iashchuk.com>
2017-12-06 00:56:55 +0200

    Fixed error while selecting languages

--- prestoid/SettingsViewController.swift
+++ prestoid/SettingsViewController.swift
@@ -8,5 +8,19 @@
 //  Application owner - Scott Leatham. All rights reserved.
 //
 
-import Foundation
 import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
