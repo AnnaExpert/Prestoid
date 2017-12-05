f3e7b08145db8bcdc09e4abd0ebc38e8dea2105c
iAlexander <alexander@iashchuk.com>
2017-12-05 18:16:55 +0200

    Added settings view

---
+++ prestoid/SettingsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewController.swift
+//  prestoid
+//
+//  Created by Alexander Iashchuk on 5/12/17.
+//  Copyright © 2017 Alexander Iashchuk. All rights reserved.
+//
+
+import Foundation
