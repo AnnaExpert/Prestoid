79a7fdae19d2ff26c87f780f41260c4d85b532d6
iAlexander <alexander@iashchuk.com>
2017-12-05 18:20:12 +0200

    Updated build version for cellular 3G tests

--- prestoid/SettingsViewController.swift
+++ prestoid/SettingsViewController.swift
@@ -1,7 +1,7 @@
 //
 //  SettingsViewController.swift
 //  Prestoid - Dropbox sync video camera app with speech to text recognition
-//  Application version 2.0, build 13, 2017.12.05
+//  Application version 2.0, build 11, 2017.12.05
 //
 //  Created by Alexander Iashchuk on 5/12/17.
 //  Copyright © 2016 Alexander Iashchuk (iAlexander), http://iashchuk.com
